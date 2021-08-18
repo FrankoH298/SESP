9b6731116aed82cc0fba4637d9cf694c156be6c1
FrankoH298 <francoh298@gmail.com>
2021-08-17 22:42:02 -0300

    cambios en frontend y settings.pu

--- SESP/sesp-frontend/src/App.js
+++ SESP/sesp-frontend/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 
+import Navbar from "./components/Navbar/Navbar"
+import "materialize-css/dist/css/materialize.css"
+
 function App() {
   return (
     <>
+      <Navbar />
       <h1 className="dummy-title">Hola</h1>
     </>
   );
