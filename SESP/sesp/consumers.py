d466e6e24c4b1f11a9db57a4da1cd4e946fc7da8
FrankoH298 <francoh298@gmail.com>
2021-08-18 22:22:16 -0300

    Realtime actual people in html

--- SESP/sesp/consumers.py
+++ SESP/sesp/consumers.py
@@ -1,4 +1,5 @@
 from channels.generic.websocket import WebsocketConsumer
+import channels.layers
 from asgiref.sync import async_to_sync
 import copy, json
 
@@ -13,7 +14,10 @@ class WSConsumer(WebsocketConsumer):
     
     def receive(self, text_data=None, bytes_data=None):
         async_to_sync(self.channel_layer.group_add)(text_data, self.channel_name) # Add user to group
-        async_to_sync(self.channel_layer.group_send)("1", {'type': 'send_number', 'number': 1})
 
     def send_number(self, event):
         self.send(text_data=json.dumps(event['number'])) # Send message to WebSocket
+    
+    def send_number_to_group(group, text):
+        channel_layer = channels.layers.get_channel_layer()
+        async_to_sync(channel_layer.group_send)(str(group), {'type': 'send_number', 'number': text})
--- SESP/sesp/models.py
+++ SESP/sesp/models.py
@@ -5,27 +5,18 @@ from django.db.models.fields import CharField
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib import admin
+from .consumers import WSConsumer
 
 class Store(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=50)
     telephone_number = models.CharField(max_length=100, null=True)
     max_people = models.SmallIntegerField(default=0) # Default 0 porque a la hora de crear un user es nulo y no puede calcular is_full
+    actual_people = models.SmallIntegerField(default=0)
     #address = models.ForeignKey("Address", verbose_name=("Direccion"), on_delete=models.CASCADE, null=True)
-    
-    @property
-    def actual_people(self): #Geteo de todas las instancias, poco eficaz, posible eliminacion
-        entries = len(Entry.objects.filter(store=self))
-        exits = len(Exit.objects.filter(store=self))
-        # Si me da negativo (hubo entradas no notificadas) devuelvo 0 para evitar errores
-        actual_people = entries - exits
-        if actual_people < 0:
-            actual_people = 0
-
-        return actual_people
 
     @property
-    def is_full(self):  # Geteo de todas las instancias, poco eficaz, posible eliminacion
+    def is_full(self):
         return self.actual_people >= self.max_people
 
     class Meta:
@@ -77,3 +68,18 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.store.save()
+
+@receiver(post_save, sender=Entry)
+def update_store_entries(sender, instance, created, **kwargs):
+    if created:
+        if not instance.store.is_full:
+            instance.store.actual_people += 1
+            instance.store.save()
+            WSConsumer.send_number_to_group(1, instance.store.actual_people)
+
+@receiver(post_save, sender=Exit)
+def update_store_exits(sender, instance, created, **kwargs):
+    if created:
+        instance.store.actual_people -= 1
+        instance.store.save()
+        WSConsumer.send_number_to_group(1, instance.store.actual_people)
