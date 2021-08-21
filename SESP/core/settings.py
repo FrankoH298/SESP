567ebcd6511434af181e59b97237b805db973f07
FrankoH298 <francoh298@gmail.com>
2021-08-21 00:58:29 -0300

    fixed page 404 not found

--- SESP/core/settings.py
+++ SESP/core/settings.py
@@ -156,3 +156,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REACT_ROUTES = [
+    'sucursal-info'
+]
--- SESP/sesp/urls.py
+++ SESP/sesp/urls.py
@@ -3,7 +3,8 @@ from .views import *
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-
+from django.conf import settings
+from django.views.generic import TemplateView
 
 router = DefaultRouter()
 router.register(r'entries', EntryViewSet)
@@ -22,3 +23,10 @@ urlpatterns = [
      path('api/total_entries_by_month/', total_entries_by_month, name='total_entries_by_month'),
      path('api/total_entries_last_week/', total_entries_last_week, name='total_entries_last_week'),
 ]
+
+react_routes = getattr(settings, 'REACT_ROUTES', [])
+
+for route in react_routes:
+    urlpatterns += [
+        path('{}'.format(route), TemplateView.as_view(template_name='index.html'))
+    ]
