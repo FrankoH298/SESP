d24fd05669427f10ac7c7382e6708122d6537998
FrankoH298 <francoh298@gmail.com>
2021-08-22 14:12:32 -0300

    allowed CORS

--- SESP/core/settings.py
+++ SESP/core/settings.py
@@ -165,8 +165,8 @@ REACT_ROUTES = [
     '',
 ]
 
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:3000',
-# ]
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
 
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
