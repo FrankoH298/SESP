8a615082f01ac523034c0f7027041dbd914fcbee
BenjaCimatti <benja.cimatti@gmail.com>
2021-08-23 11:15:38 -0300

    Fix

--- sesp/routing.py
+++ sesp/routing.py
@@ -1,7 +1,18 @@
 from django.urls import path
-
+from core.wsgi import *  # add this line to top of your code
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from .consumers import WSConsumer
 
 ws_urlpatterns = [
     path('ws/inicio/', WSConsumer.as_asgi())
 ]
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            ws_urlpatterns
+        )
+    ),
+})
